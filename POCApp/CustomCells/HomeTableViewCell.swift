76ac665f0e09b07a0f7df1f4f5a56495a4b96bf5
Aakash Jaiswal <ajaiswal3@apple.com>
2020-11-09 12:06:55 +0530

    Add home screen controller and related table view custom cell file

---
+++ POCApp/CustomCells/HomeTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  HomeTableViewCell.swift
+//  POCApp
+//
+//  Created by Ritesh on 07/11/20.
+//  Copyright © 2020 Ritesh. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    let wikiImageView = UIImageView()
+    let titleLabel = UILabel()
+    let descriptionLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //Constraints for labels in cell
+    func setupConstraints() {
+        wikiImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(wikiImageView)
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
+        
+        wikiImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        wikiImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        wikiImageView.widthAnchor.constraint(equalToConstant: 58).isActive = true
+        wikiImageView.heightAnchor.constraint(equalToConstant: 58).isActive = true
+        
+        titleLabel.leadingAnchor.constraint(equalTo: wikiImageView.trailingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 15).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        
+        descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
+        descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+}
---
+++ POCApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,170 @@
+//
+//  HomeViewController.swift
+//  POCApp
+//
+//  Created by Ritesh on 07/11/20.
+//  Copyright © 2020 Ritesh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    let tableView = UITableView()
+    
+    var rowData = [Row]()
+    var refreshControl = UIRefreshControl()
+
+    @objc func refresh(_ sender: AnyObject) {
+        tableView.reloadData()
+        self.refreshControl.endRefreshing()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControlFunc()
+        configureTableView()
+        setupConstraints()
+        fetchHomeData()
+    }
+    
+    //Function for Pull To Referesh
+    func refreshControlFunc() {
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
+        tableView.addSubview(refreshControl)
+    }
+    
+    //Configure table view properties
+    func configureTableView() {
+        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: "homeRow")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 50.0
+        tableView.rowHeight = UITableView.automaticDimension
+        view.addSubview(tableView)
+    }
+    
+    //Set table view constraints
+    func setupConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+
+    //Fetch data
+    func fetchHomeData() {
+        self.fetchHomeDetails { (homeData, error) in
+            DispatchQueue.main.async {
+                guard let homeData = homeData else {
+                    self.showAlert(withMessage: "Request Failed")
+                    return
+                }
+                self.displayHomeData(data: homeData)
+            }
+        }
+    }
+    
+    //Display data
+    func displayHomeData(data: Home) {
+        title = data.title
+        rowData = data.rows
+        tableView.reloadData()
+    }
+    
+    func showAlert(withMessage message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true)
+    }
+    
+    func fetchImage(indexPath: IndexPath) {
+        let imageURL = rowData[indexPath.row].imageURL ?? ""
+        if let url = URL(string: imageURL) {
+            self.fetchWikiImage(url: url) { [unowned self] (image, error) in
+                DispatchQueue.main.async {
+                    guard let image = image else {
+                        return
+                    }
+                    self.rowData[indexPath.row].image = image
+                    self.tableView.reloadRows(at: [indexPath], with: .none)
+                }
+            }
+        }
+    }
+}
+
+extension HomeViewController {
+    func fetchHomeDetails(_ callback: ((Home?, Error?) -> Void)?) {
+        guard let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts") else {
+            callback?(nil, NetworkError.invalidRequest)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                callback?(nil, error)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                 httpResponse.statusCode == 200,
+                 let data = data,
+                 let response = try? JSONDecoder().decode(Home.self, from: (String(data: data, encoding: .isoLatin1)?.data(using: .utf8))!) else {
+                callback?(nil, NetworkError.invalidResponse)
+                return
+            }
+            callback?(response, nil)
+        }
+        task.resume()
+    }
+    
+    func fetchWikiImage(url: URL, _ callback: ((UIImage?, Error?) -> Void)?) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                callback?(nil, NetworkError.invalidResponse)
+                return
+            }
+            let image = UIImage(data: data)
+            callback?(image, nil)
+        }
+        task.resume()
+    }
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rowData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "homeRow", for: indexPath) as? HomeTableViewCell else {
+            return HomeTableViewCell()
+        }
+        setupCell(atIndexPath: indexPath, cell: cell)
+        return cell
+    }
+    
+    func setupCell(atIndexPath indexPath: IndexPath, cell: HomeTableViewCell) {
+        let row = rowData[indexPath.row]
+        cell.titleLabel.text = row.title
+        cell.descriptionLabel.text = row.rowDescription
+        cell.descriptionLabel.numberOfLines = 0
+        cell.descriptionLabel.lineBreakMode = .byWordWrapping
+        cell.wikiImageView.image = row.image
+        if cell.wikiImageView.image == nil {
+            fetchImage(indexPath: indexPath)
+        }
+    }
+}
+
+enum NetworkError: Error {
+    case invalidRequest
+    case invalidResponse
+}
